d91c7adc8e09ddba74d8d5b2f760f5927434f1bc
haraldosan <67067769+haraldosan@users.noreply.github.com>
2020-10-02 15:00:25 +0200

    Update Shrekbot.py

--- ShrekBot/Shrekbot.py
+++ ShrekBot/Shrekbot.py
@@ -3,6 +3,7 @@ from fbchat import Client
 from fbchat.models import *
 import os
 import time
+import sys
 
 email = os.environ["email"]
 fbpass = os.environ["fbpassord"]
@@ -13,28 +14,37 @@ client = Client(email, fbpass)
 print("Own id: {}", format(client.uid))
 
 threads = client.fetchThreadList()
+try:
 
+    navn = input("Velg facebookvenn: ")
+    user = client.searchForUsers(navn)[0]
 
-navn = input("Velg facebookvenn: ")
-user = client.searchForUsers(navn)[0]
+    print("user ID: {}".format(user.uid))
+    print("user's name: {}".format(user.name))
+    print("user's photo: {}".format(user.photo))
+    print("Is user client's friend: {}".format(user.is_friend))
+    print(type(user.is_friend))
 
-print("user ID: {}".format(user.uid))
-print("user's name: {}".format(user.name))
-print("user's photo: {}".format(user.photo))
-print("Is user client's friend: {}".format(user.is_friend))
+    fil = "ballefrans.txt"
 
+    with open(fil, "r") as file:
+        tekst = file.read()
+        newfile = tekst.split("\n")
 
-fil = "ballefrans.txt"
+    moviescript = []
 
+    for i in range(len(newfile)):
+        if newfile[i] == " " or newfile[i] == "":
+            continue
+        else:
+            moviescript.append(newfile[i])
 
-with open(fil, "r") as file:
-    tekst = file.read()
-    newfile = tekst.split("\n")
+    for i in range(25):
+        client.send(
+            Message(text=moviescript[i]), thread_id=user.uid, thread_type=ThreadType.USER)
+        time.sleep(3)
 
-for i in range(10):
-    client.send(
-        Message(text=str(newfile[i])), thread_id=user.uid, thread_type=ThreadType.USER)
-    time.sleep(3)
+    client.logout()
 
-
-client.logout()
+except:
+    sys.exit()
